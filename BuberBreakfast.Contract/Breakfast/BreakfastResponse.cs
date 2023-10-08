f0b110ba5f9563dcd5d467d75bd4ad13a92bade6
Zhimao Lin <zhimaolin@outlook.com>
2023-10-08 17:08:18 -0400

    Added BuberBreakfast contract

---
+++ BuberBreakfast.Contract/Breakfast/BreakfastResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuberBreakfast.Contract.Breakfast
+{
+    public record BreakfastResponse
+    {
+        public Guid Id { get; init; }
+        public string Name { get; init; }
+        public string Description { get; init; }
+        public DateTime StartDateTime { get; init; }
+        public DateTime EndDateTime { get; init; }
+        public DateTime LastModifiedDateTime { get; init; }
+        public List<string> Savory { get; init; }
+        public List<string> Sweet { get; init; }
+    }
+}
---
+++ BuberBreakfast.Contract/Breakfast/CreateBreakfastRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuberBreakfast.Contract.Breakfast
+{
+    public record CreateBreakfastRequest
+    {
+        public string Name { get; init; }
+        public string Description { get; init; }
+        public DateTime StartDateTime { get; init; }
+        public DateTime EndDateTime { get; init; }
+        public List<string> Savory { get; init; }
+        public List<string> Sweet { get; init; }
+    }
+}
