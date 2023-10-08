f56e13dd64d603c6ca9c4d53a64ccf2d7b55e4d3
Zhimao Lin <zhimaolin@outlook.com>
2023-10-08 17:08:37 -0400

    Added BreakfastController

---
+++ BuberBreakfast/Controllers/BreakfastsController.cs
@@ -0,0 +1,35 @@
+﻿using BuberBreakfast.Contract.Breakfast;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberBreakfast.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BreakfastsController : ControllerBase
+    {
+        [HttpPost()]
+        public IActionResult CreateBreakfast(CreateBreakfastRequest request)
+        {
+            return Ok(request);
+        }
+
+        [HttpGet("{id:Guid}")]
+        public IActionResult GetBreakfast(Guid id)
+        {
+            return Ok(id);
+        }
+
+        [HttpPut("{id:Guid}")]
+        public IActionResult UpsertBreakfast(Guid id, UpsertBreakfastRequest request)
+        {
+            return Ok(request);
+        }
+
+        [HttpDelete("{id:Guid}")]
+        public IActionResult DeleteBreakfast(Guid id)
+        {
+            return Ok(id);
+        }
+    }
+}
